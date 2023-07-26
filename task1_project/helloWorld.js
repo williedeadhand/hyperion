d56e5cf31bc504f291ac127c4e75974ff02e8eb2
Wpudyane <a.titanhive@gmail.com>
2023-07-26 20:45:37 +0200

    these are things i feel would improve javascript a bit

--- task1_project/helloWorld.js
+++ task1_project/helloWorld.js
@@ -1,3 +1,8 @@
 let yourInput = prompt("type something")
 
 console.log(yourInput)
+
+// github username williedeadhand
+
+// I think the should be a new method above the splice function for arrays that is easier to use 
+// const should be made to only work within a function to since var and let exist  
